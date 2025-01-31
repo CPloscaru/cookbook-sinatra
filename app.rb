b95c29eebbc0780e3a7ca4e59d4b8b9e876c8109
Calin Ploscaru <ytcognito@gmail.com>
2025-01-31 14:34:47 +0100

    add comments

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 
-get "/" do
+get "/" do # <- Router part
   "Hello world!"
+
+  # [...]   #
+  # [...]   # <- Controller part
+  # [...]   #
 end
